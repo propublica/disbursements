182058c7cbdd9c8fb84cbae73df1390d49ceda8b
Eric Mill <kprojection@gmail.com>
2010-10-25 12:32:56 -0400

    Pulling out unique offices from positions.csv and appending them to offices.csv

---
+++ staffers/3_offices.rb
@@ -0,0 +1,48 @@
+#!/usr/bin/env ruby
+
+# will find the unique original office names in positions.csv and append any unseen ones to the end of offices.csv
+
+positions_file = "data/positions.csv"
+offices_file = "data/offices.csv"
+
+unless File.exists?(positions_file)
+  puts "Couldn't locate #{positions_file}. Place it in the same directory as this script."
+  exit
+end
+
+unless File.exists?(offices_file)
+  puts "Couldn't locate #{offices_file}. Place it in the same directory as this script."
+  exit
+end
+
+begin
+  require 'fileutils'
+  require 'rubygems'
+  require 'fastercsv'
+rescue
+  puts "Couldn't load FasterCSV. Try running \"sudo gem install fastercsv\" and try again."
+  exit
+end
+
+offices = []
+
+FasterCSV.foreach(offices_file) do |row|
+  next if row[0] == "OFFICE NAME (ORIGINAL)" # header row of offices.csv
+  offices << row[0]
+end
+
+i = 0
+FasterCSV.open(offices_file, "a") do |csv|
+  
+  FasterCSV.foreach(positions_file) do |row|
+    next if row[0] == "STAFFER NAME (ORIGINAL)" # header row of positions.csv
+    next if row[3] != nil and row[3] != "" # has a bioguide ID, we'll handle them separately
+    next if offices.include?(row[4])
+    
+    csv << [row[4], row[4]]
+    offices << row[4]
+    i += 1
+  end
+end
+
+puts "Appended #{i} new offices to #{offices_file}."
