34b329342dad777bc8af9880d7392d02ddd35fb4
Eric Mill <kprojection@gmail.com>
2011-06-02 14:11:33 -0400

    Stripping out OVERTIME and OTHER COMPENSATION marks on titles

--- staffers/4_titles.rb
+++ staffers/4_titles.rb
@@ -36,9 +36,17 @@ FasterCSV.open(titles_file, "a") do |csv|
   
   FasterCSV.foreach(positions_file) do |row|
     next if row[0] == "STAFFER NAME (ORIGINAL)" # header row in positions.csv
+    row[1] ||= ""
+    
+    # strip off common addendums
+    ["(OTHER COMPENSATION)", "(OVERTIME)"].each do |addendum|
+      row[1].sub! addendum, ''
+    end
+    row[1].strip!
+    
     next if titles.include?(row[1])
     
-    csv << [row[1], row[1]]
+    csv << [row[1], '']
     titles << row[1]
     i += 1
   end
