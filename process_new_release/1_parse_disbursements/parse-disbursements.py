875944a02353234996f7c898becedde59ddf56d1
Eric Mill <kprojection@gmail.com>
2012-01-05 11:45:31 -0500

    Merged in Luke's longstanding pull request to fix bugs around ditto parsing, and to support newer transaction codes, and added a bugfix of my own to certain benefits rows

--- process_new_release/1_parse_disbursements/parse-disbursements.py
+++ process_new_release/1_parse_disbursements/parse-disbursements.py
@@ -2,35 +2,25 @@
 # -*- coding: utf-8 -*-
 
 '''
-    This script was used to convert the 2009 Q3 House Disbursement PDF into detail and summary CSV files.
+    This script is used to convert House Disbursement PDFs into detail and summary CSV files.
         Source PDF: http://disbursements.house.gov/
         Resulting Data: http://www.sunlightfoundation.com/projects/2009/expenditures/
 
     It was originally authored by Luke Rosiak with improvements by James Turk for Sunlight Labs and is released into the public domain.
 
-    Disclaimer: It was written quickly under deadline and likely contains a few bugs - patches welcome
-
-    It expects a file (named in the disbursements_file variable below) created as the result of something like the following two operations:
+    It expects a file (named in the disbursements_file variable below) created as the result of something like the following operation:
        
-       pdftk 2010q1_singlevolume.pdf cat 10-3613 output disbursements-only.pdf
-       
-       pdftotext -layout disbursements-only.pdf
+       pdftotext -layout 2010q1_singlevolume.pdf
 
-
-    Update the:
-      * disbursements_file variable (input file, e.g. '2010q2-disbursements-only.txt')
-      * year (e.g. '2009', '2010')
-      * quarter (e.g. 'Q1', 'Q2') 
-   
-   WARNING: there's a hardcoded or clause with individual years in it - this should be refactored to be an array, or at least kept up to date. I added up to 2015.
+    The filename's first six characters must represent the quarter it covers. The file is passed to this script as its only argument:
+    
+       python parse-disbursements.py 2010q1_singlevolume.txt
+      
+  
 '''
 
-disbursements_file = '2011q3-disbursements-only.txt'
-year = '2011'
-quarter = 'Q3'
-thisquarter = year + quarter
 
-import csv, re, sys
+import csv, re, sys, os
 
 BAD_LINE_RE = re.compile('^(Frm|Fmt|Sfmt|Jkt|VerDate|VOUCHER|OFFICIAL\sEXPENSES|MEMBERS\sREPRESENATION\sALLOW|PO|APPS06|PsN:|07:23|M:)')
 
@@ -38,7 +28,13 @@ BAD_LINE_RE = re.compile('^(Frm|Fmt|Sfmt|Jkt|VerDate|VOUCHER|OFFICIAL\sEXPENSES|
 def known_bad(line):
     return (not line) or BAD_LINE_RE.match(line) or 'dkrause' in line
 
-def main():
+def main(disbursements_file):
+    
+    path, filename = os.path.split(disbursements_file)
+    year = filename[:4]
+    quarter = filename[4:6].upper()
+    thisquarter = year + quarter
+    
     f = open(disbursements_file, "r")
 
     fsummary = csv.writer(open("%s%s-house-disburse-summary.csv" % (year, quarter), "w"), quoting=csv.QUOTE_ALL)
@@ -55,39 +51,38 @@ def main():
 
     oldrecip = None
 
-    regular_re = re.compile(r"""(\d{2}-\d{2})\s+            # date
-                            ([0-9A-Z]{2})\s+                # transaction code
-                            ([0-9A-Z\-]+)\s+                  # record id
-                            (.*?)                           # recipient
-                            (\d{2}/{1}\d{2}/{1}\d{2})\s+    # date-start
-                            (\d{2}/{1}\d{2}/{1}\d{2})       # date-end
-                            (.*?)\s+                        # description
-                            (-?[0-9,]+\.\d{2})              # amount
+    regular_re = re.compile(r"""(\d{2}-\d{2})[\s\.]+            # date
+                            (GL\sLAW|GL\sFRM|\w{2})[\s\.]+       # transaction code
+                            ([0-9A-Z\-]+)[\s\.]+                # record id
+                            (.*?)[\s\.]+                        # recipient
+                            (\d{2}/{1}\d{2}/{1}\d{2})[\s\.]+    # date-start
+                            (\d{2}/{1}\d{2}/{1}\d{2})[\s\.]+    # date-end
+                            (.*?)[\s\.]+                        # description
+                            (-?[0-9,]+\.\d{2})                  # amount
                             """, re.VERBOSE)
-    personel_re = re.compile(r"""(.*?)                      # recipient
-                             (\d{2}/{1}\d{2}/{1}\d{2})\s+   # date-start
-                             (\d{2}/{1}\d{2}/{1}\d{2})      # date-end
-                             (.*?)\s+                       # description
-                             (-?[0-9,]+\.\d{2})             # amount
+    personnel_re = re.compile(r"""(.*?)[\s\.]+                  # recipient
+                             (\d{2}/{1}\d{2}/{1}\d{2})[\s\.]+   # date-start
+                             (\d{2}/{1}\d{2}/{1}\d{2})[\s\.]+   # date-end
+                             (.*?)[\s\.]+                       # description
+                             (-?[0-9,]+\.\d{2})                 # amount
                              """, re.VERBOSE)
-    summary_re = re.compile(r"""(.*?)\.+\s+         # category
-                            (-?[0-9,]+\.\d{2})\s+   # 2009
-                            (-?[0-9,]+\.\d{2})      # 2009-Q3
+    summary_re = re.compile(r"""(.*?)[\s\.]+                    # category
+                            (-?[0-9,]+\.\d{2})\s+               # 2009
+                            (-?[0-9,]+\.\d{2})                  # 2009-Q3
                             """, re.VERBOSE)
 
 
-    benefits_re = re.compile(r"""(\d{2}-\d{2})\s+            # date
-                            ([0-9A-Z]{2})\s+                # transaction code
-                            ([0-9A-Z]+)\s+                  # record id
-                            (.*?)                           # recipient
-                            \s+    # no dates
-                            (.*?)\s+                        # description
-                            (-?[0-9,]+\.\d{2})              # amount
+    benefits_re = re.compile(r"""(\d{2}-\d{2})[\s\.]+           # date
+                            (GL\sLAW|GL\sFRM|\w{2})[\s\.]+       # transaction code
+                            ([0-9A-Z]+)[\s\.]+                  # record id
+                            (.*?)                               # recipient
+                            [\s\.]+                             # no dates
+                            (.*?)[\s\.]+                        # description
+                            (-?[0-9,]+\.\d{2})                  # amount
                             """, re.VERBOSE)
 
 
-
-    ditto_re = re.compile(r"""\s*DO[\s\.]*""")
+    year_re = re.compile(r"""(FISCAL YEAR\s+)?20([01]\d)\s+(\w+.*)\s*""")
 
 
     transcodes = {
@@ -117,7 +112,15 @@ def main():
 'S5': 'Telecommunication services',
 'S6': 'District Office rent in space leased from GSA',
 'S7': 'Transit Benefits',
-'S8': 'equipment plan or maintenance provided through the CAO' } 
+'S8': 'equipment plan or maintenance provided through the CAO',
+
+#less detailed codes, used for newer quarters :(
+'AP': 'Accounts payable',
+'AR': 'Accounts receivable', 
+'GL': 'General ledger',
+'GL LAW': 'charges for purchase of services for Public Law documentation',
+'GL FRM': 'Interface charge for goods purchased through Framing Office'
+ } 
 
 
     for l in f.readlines():
@@ -128,8 +131,7 @@ def main():
         
 
         # new member
-        #TODO: refactor this to use an array
-        if l.startswith("2015 ") or l.startswith("2014 ") or l.startswith("2013 ") or l.startswith("2012 ") or l.startswith("2011 ") or l.startswith("2010 ") or l.startswith("2008 ") or l.startswith("2009 ") or l.startswith('2007 ') or l.startswith("FISCAL YEAR "):
+        if year_re.match(l):
             if l.startswith("FISCAL YEAR "):
                 thismem = l.replace('—', '')[17:]
                 thisyear = l[:16]
@@ -138,6 +140,8 @@ def main():
                 thisyear = l[:4]
             if thismem.endswith("Con."):
                 thismem = thismem[:-4]
+            thismem = thismem.strip()
+            thisyear = thisyear.strip()
             continue
 
         # category
@@ -146,15 +150,15 @@ def main():
             continue
 
         #regular record
-        ma = regular_re.search(l)
+        ma = regular_re.match(l)
         if ma:
             m = ma.groups()
-            date1 = m[0].replace('–', '-')
+            date1 = m[0]
             transcode = m[1]
             recordid = m[2]
-            recip = m[3].strip().rstrip('.').rstrip('\t')
+            recip = m[3]
             sunrecip = recip
-            if ditto_re.match(recip):
+            if recip=='DO': #DO for 'Ditto last line' is not used in newer filings.
                 sunrecip = oldrecip
             else:
                 oldrecip = recip
@@ -162,33 +166,31 @@ def main():
                 sunrecip=recip
             date2 = m[4]
             date3 = m[5]
-            descrip = m[6].strip().rstrip('.')
+            descrip = m[6]
             amount = m[7]
 
             transcodelong = ''
             if transcode in transcodes:
                 transcodelong = transcodes[transcode]
 
-            #fdetail.writerow([thismem, thisquarter, thisyear, thiscat, date1, transcode, recordid, sunrecip, recip, date2, date3, descrip, amount])
+
             fdetail.writerow([thismem, thisquarter, thiscat, date1, sunrecip, date2, date3, descrip, amount, thisyear, transcode, transcodelong, recordid, recip])
             continue
 
-        # personell record
-        ma = personel_re.search(l)
+        # personnel record
+        ma = personnel_re.match(l)
         if ma:
             m = ma.groups()
-            recip = m[0].strip().rstrip('.')
+            recip = m[0]
             sunrecip = recip
-            if ditto_re.match(recip):
+            if recip=='DO': #DO for 'Ditto last line' is not used in newer filings.
                 sunrecip = oldrecip
             else:
                 oldrecip = recip
             date2 = m[1]
             date3 = m[2]
-            descrip = m[3].strip().rstrip('.')
+            descrip = m[3]
             amount = m[4]
-            #fdetail.writerow( ['OFFICE','QUARTER','CATEGORY','DATE','PAYEE','START DATE','END DATE','PURPOSE','AMOUNT','YEAR', 'TRANSCODE','TRANSCODELONG','RECORDID','RECIP (orig.)'] )
-            #fdetail.writerow([thismem, thisquarter, thisyear, thiscat, "", "", "", sunrecip, recip, date2, date3, descrip, amount])
             fdetail.writerow([thismem, thisquarter, thiscat, "", sunrecip, date2, date3, descrip, amount, thisyear, "","","", recip ])
             continue
 
@@ -196,28 +198,31 @@ def main():
 
 
         # benefits record
-        ma = benefits_re.search(l)
+        ma = benefits_re.match(l)
         if ma:
             m = ma.groups()
-            recip = m[3].strip().rstrip('.')
+            recip = m[3]
             sunrecip = recip
-            if ditto_re.match(recip):
+            if recip=='DO': #DO for 'Ditto last line' is not used in newer filings.
                 sunrecip = oldrecip
             else:
                 oldrecip = recip
-            descrip = m[4].strip().rstrip('.')
+            descrip = m[4]
             amount = m[5]
-            date1 = m[0].replace('–', '-')
+            date1 = m[0]
             transcode = m[1]
             recordid = m[2]
             transcodelong = ''
             if transcode in transcodes:
                 transcodelong = transcodes[transcode]
 
+            # benefits rows don't really have a recipient, this is to kludge around a hard-to-fix bug in benefits_re
+            if thiscat == "PERSONNEL BENEFITS":
+                descrip = ("%s %s" % (sunrecip, descrip)).strip() # leaves the space in if sunrecip had a value, strips it if not
+                sunrecip = ""
 
-            #fdetail.writerow( ['OFFICE','QUARTER','CATEGORY','DATE','PAYEE','START DATE','END DATE','PURPOSE','AMOUNT','YEAR', 'TRANSCODE','TRANSCODELONG','RECORDID','RECIP (orig.)'] )
-            print [thismem, thisquarter, thiscat, date1, sunrecip, "","", descrip, amount, thisyear, transcode,transcodelong,recordid, recip ]
             fdetail.writerow([thismem, thisquarter, thiscat, date1, sunrecip, "","", descrip, amount, thisyear, transcode,transcodelong,recordid, recip ])
+            
             continue
 
 
@@ -225,7 +230,7 @@ def main():
 
 
         # summary record
-        ma = summary_re.search(l)
+        ma = summary_re.match(l)
         if ma:
             m = ma.groups()
             if m[0].strip() in cats:
@@ -236,4 +241,5 @@ def main():
             trashcan.write(l)
 
 if __name__ == '__main__':
-    main()
+    infile = sys.argv[1]
+    main(infile)
